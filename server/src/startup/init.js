f109cdd6cc49f69e5ef1b1f0a5971c383355e49e
Vitaly Dostovalov <dostovalovvs@gmail.com>
2018-09-11 20:50:26 +1200

    Create admin user if not exist

--- server/src/startup/init.js
+++ server/src/startup/init.js
@@ -1,8 +1,17 @@
-import { Account, Transaction, Budget } from '../model';
+import {
+  User, Account, Transaction, Budget,
+} from '../model';
 
 export default async function initDb(client) {
   const db = client.db();
   // COMBAK: indexes
+  const userModel = new User(db, 'admin');
+  userModel.init();
+  const admin = await userModel.findOne();
+  if (!admin) {
+    await userModel.create({ password: 'password' });
+  }
+
   const Accounts = new Account(db, 'admin');
   await Accounts.init();
 
